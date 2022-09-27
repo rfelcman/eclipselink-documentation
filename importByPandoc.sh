03cddc2ee915145cc39a72788d8f3b722af70a53
Radek Felcman <radek.felcman@oracle.com>
2022-09-27 21:26:30 +0200

    POC Helidon documentation generation

--- importByPandoc.sh
+++ importByPandoc.sh
@@ -6,7 +6,7 @@ rm -rf jpa/extensions/src/docs/asciidoc/*
 #pandoc -f html -t asciidoc -o /toc.adoc ../webSite/documentation/3.0/jpa/extensions/toc.htm
 
 export JPA_DOC_INPUT_PATH=../webSite/documentation/3.0/jpa/extensions
-export JPA_DOC_OUTPUT_PATH=jpa/extensions/src/docs/asciidoc
+export JPA_DOC_OUTPUT_PATH=docs/jpa/extensions/src/docs/asciidoc
 
 for f in $(ls ${JPA_DOC_INPUT_PATH} | grep .htm);
 do
