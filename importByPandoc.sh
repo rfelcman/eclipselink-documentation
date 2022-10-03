571d64d921c4eaf70682992a59b2e2a475f21322
Radek Felcman <radek.felcman@oracle.com>
2022-10-03 17:04:57 +0200

    Project refactor to Jakarta style from Helidon ASCII Doctor plugin is used.

--- importByPandoc.sh
+++ importByPandoc.sh
@@ -11,5 +11,6 @@ rm -rf ${JPA_DOC_OUTPUT_PATH}/*
 for f in $(ls ${JPA_DOC_INPUT_PATH} | grep .htm);
 do
   echo Processing ${JPA_DOC_INPUT_PATH}/${f} ;
-  pandoc -f html -t asciidoc -o ${JPA_DOC_OUTPUT_PATH}/${f}.adoc ${JPA_DOC_INPUT_PATH}/${f}
+  output_filename=$(echo "$f" | cut -f 1 -d '.')
+  pandoc -f html -t asciidoc -o ${JPA_DOC_OUTPUT_PATH}/${output_filename}.adoc ${JPA_DOC_INPUT_PATH}/${f}
 done;
